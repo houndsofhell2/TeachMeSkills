d3abd086c7d614c2b872193be38f9646cb19ba5a
houndsofhell2 <xxidiotnationxx@gmail.com>
2024-11-12 19:32:18 +0300

    new test file from cloned repo

---
+++ homework4/homework4.py
@@ -0,0 +1,11 @@
+#1. Переменной var _int присвойте значение 10, var float - значение 8.4, var str - "No".
+
+var_int = 10
+var_float = 8.4
+var_str = "No"
+print(var_int, type(var_int))
+print(var_float, type(var_float))
+print(var_str, type(var_str))
+
+
+#2.
